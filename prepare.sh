78c3ad4eeb4fe05d428f4a4e34ce99f35d4b6626
Andrew Curtis <80860310+meisZWFLZ@users.noreply.github.com>
2024-02-28 07:23:23 +0000

    feat: :hammer: add fonts to prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -3,6 +3,10 @@ cd notebookinator
 just install
 cd ..
 
-cargo install typst-live 
-cargo install --git https://github.com/astrale-sharp/typstfmt.git --tag 0.2.7 
+cargo install typst-live
+cargo install --git https://github.com/astrale-sharp/typstfmt.git --tag 0.2.7
 cargo install typos-cli
+
+# add fonts
+sudo mkdir /usr/share/fonts/truetype/telemarines
+sudo cp -R ./assets/fonts /usr/share/fonts/truetype/telemrines
